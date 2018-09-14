ff9960be6527abae20573738bbf7f5fe18d89e07
Bruno Fontes <developer@brunofontes.net>
2018-09-13 21:16:57 -0300

    Fixing Product buttons action
    It were pointing to /product/id but needs to point to /product.

--- resources/views/product/buttons.blade.php
+++ resources/views/product/buttons.blade.php
@@ -4,17 +4,17 @@
 
         <!-- EDIT BUTTON -->
         @if ($product->user_id == \Auth::id())
-            <form action="/product/{{$product->id}}" method="POST">
+            <form action="/product" method="POST">
                 @method('PATCH')
-                <button type="button" class="btn-sm btn-secondary mr-1">Edit</button>
+                <button type="submit" class="btn-sm btn-secondary mr-1">Edit</button>
             </form>
         @endif
 
 
         <!-- DELETE BUTTON -->
-        <form action="/product/{{$product->id}}" method="POST">
+        <form action="/product" method="POST">
             @method('DELETE')
-            <button type="button" class="btn-sm btn-danger">Delete</button>
+            <button type="submit" class="btn-sm btn-danger">Delete</button>
         </form>
 
 
