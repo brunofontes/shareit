ee9a01651dcd5b9755b7e51b4cbdf4f759c4c01b
Bruno Fontes <developer@brunofontes.net>
2018-09-14 17:00:28 -0300

    Adding a Welcome Mail

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+# Welcome, {{$user->name}},
+
+Thank's for registering at **Share It!**
+
+Your account was created and it is active. And you? Are you ready to Share It with your friends? :)
+
+@component('mail::button', ['url' => 'https://shareit.brunofontes.net'])
+Share it!
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
