7a4db2c9b762de09bc80a6d35955977c89ba82f0
Bruno Fontes <developer@brunofontes.net>
2018-09-17 15:22:49 -0300

    Fixing addProductForm and item-users responsive views

--- resources/views/item/users.blade.php
+++ resources/views/item/users.blade.php
@@ -3,35 +3,39 @@
     @if (!$loop->first)
         <hr>
     @endif
-    {{ $user->name }} ({{ $user->email}})
-    <div class="form-inline float-right">
-    <form method="POST" action="/user" class="form-inline">
-        <div class="form-group">
-            {{ csrf_field() }}
-            @method('DELETE')
-            <input type="hidden" class="form-control" name="user_id" id="user_id" value="{{$user->id}}">
-            <input type="hidden" name="item_id" id="item_id" value="{{ $item['id'] }}">
-            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+    <div class="container-fluid">
+        <div class="form-group row mr-1">
+            <div class="col-xs-12 col-sm-10 col-lg-11 mb-2">
+                {{ $user->name }} ({{ $user->email}})
+            </div>
+            <div class="col-xs-12 col-sm-2 col-lg-1">
+                <form method="POST" action="/user">
+                {{ csrf_field() }}
+                @method('DELETE')
+                <input type="hidden" name="item_id" id="item_id" value="{{ $item['id'] }}">
+                <input type="hidden" class="form-control" name="user_id" id="user_id" value="{{$user->id}}">
+                <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+            </div>
         </div>
-    </form>
     </div>
+    </form>
 @empty
     <p>There are no items yet. Include one with the form above.</p>
 @endforelse
 
 
 <!-- ADD USERS -->
-<div class="mt-5 mb-5">
-<hr>
-    <p><strong>Add user</strong></p>
-    <form method="POST" action="/user" class="form-inline">
-    <div class="form-group">
+<hr class="mt-5">
+<p><strong>Add user</strong></p>
+<form method="POST" action="/user">
+<div class="container-fluid">
+    <div class="form-group row mt-2">
         {{ csrf_field() }}
-        <div class="col"><label for="name">E-mail: </label></div>
-        <div class="col-6"><input type="email" class="form-control" name="email" id="email" placeholder="name@domain.com" required></div>
+        <div class="col-sm-2 col-lg-1"><label for="name">E-mail: </label></div>
+        <div class="col-xs-12 col-sm mb-3"><input type="email" class="form-control" name="email" id="email" placeholder="name@domain.com" required></div>
         <input type="hidden" name="item_id" id="item_id" value="{{ $item['id'] }}" required>
-        <div class="col ml-3"><button type="submit" class="btn btn-primary">Insert</button></div>
+        <div class="col-sm-2 col-lg-1 mr-4"><button type="submit" class="btn btn-primary">Insert</button></div>
     </div>
-    </form>
 </div>
+</form>
 @include ('layouts.errors')
--- resources/views/product/addProductForm.blade.php
+++ resources/views/product/addProductForm.blade.php
@@ -7,12 +7,11 @@
     </div>
     <div class="form-group row mt-2">
         <div class="col-sm-2 col-lg-1"><label for="product">URL:</label></div>
-        <div class="col-xs-12 col-sm"><input type="text" class="form-control" name="url" id="url" placeholder="http://bookwebsite.com"></div>
+        <div class="col-xs-12 col-sm"><input type="text" class="form-control" name="url" id="url" placeholder="(Optional) http://bookwebsite.com"></div>
     </div>
     <div class="form-group row mt-2">
         <div class="col-xs-0 col-sm-1 col-md-1 col-lg-1 col-xg-1"></div>
         <div class="col col-xs-12"><button type="submit" class="btn btn-primary">Insert</button></div>
-        
     </div>
 </div>
 @include ('layouts.errors')
