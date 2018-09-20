61b54ecdd806371ebe5204478c963dd8c2b0f447
Bruno Fontes <developer@brunofontes.net>
2018-09-20 20:37:21 -0300

    Changing home layout from FLOAT to GRID

--- resources/views/home/unusedItem.blade.php
+++ resources/views/home/unusedItem.blade.php
@@ -1,7 +1,5 @@
-<span class="float-right">
-    <form action="/take" method="POST">
-        {{ csrf_field() }}
-        <input type="hidden" name="item" value="{{$item->id}}"> 
-        <button type="submit" class="btn btn-sm btn-success">Take It</button>
-    </form>
-</span>
+<form action="/take" method="POST">
+    {{ csrf_field() }}
+    <input type="hidden" name="item" value="{{$item->id}}">
+    <button type="submit" class="btn btn-sm btn-success">Take It</button>
+</form>
