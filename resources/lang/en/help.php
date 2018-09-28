6b20397e9d4f6042245f72ec72ffdc38c7d2cf84
Bruno Fontes <developer@brunofontes.net>
2018-09-28 19:14:29 -0300

    Help Page translated into PTB
    Now the entire website should be translated into PTB.

--- resources/lang/en/help.php
+++ resources/lang/en/help.php
@@ -26,42 +26,48 @@ return [
     /**
      * Step-by-step
      */
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
+    'step_title' => 'Step-by-step',
+    'step_sharing_subtitle' => 'Sharing a product/item',
+    'step_sharing_steps' => '<li>Go to the <strong><a href="/product">Products</a></strong> page;</li>
+                            <li>Include the Product and click on it;</li>
+                            <li>Include an Item that belongs to that Product and click on it;</li>
+                            <li>Add other people with their subscribed e-mail address.</li>',
+
+    'step_sharedItem_subtitle' => 'Using a shared item',
+    'step_sharedItem_steps' => '<li>Go to the <strong><a href="/home">Home</a></strong> page (tip: add this page as a bookmark);</li>
+                            <li>Click on <strong>Take</strong> to take the item you want to use;</li>
+                            <li>When you finish using it, click on <strong>Return</strong> button.</li>',
+
+    'step_getAlerted_subtitle' => 'Getting alerted when an item is available',
+    'step_getAlerted_steps' => '<li>Go to the <strong><a href="/home">Home</a></strong> page (tip: add this page as a bookmark);</li>
+                            <li>Click on <strong>Alert me</strong> next the taken item;</li>
+                            <li>The active user will be alerted you want to use it later. When the person return it, you will be notified.</li>',
      
     /**
      * Screens
      */
-    '' => '',
+    'screens_title' => 'Screens',
      
     /**
      * Home
      */
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
+    'home_title' => 'Home',
+    'home_body' => '<p>The <strong>Home</strong> is the main screen where you will going to <strong>Take</strong> 
+                    and <strong>Return</strong> items.</p>
+                    <p>It shows all items that were shared with you or that you are sharing, unless you remove yourself from there.</p>
+                    <p>If the item has a website, the items name will become a link, so you can just click on it to open.</p>
+                    <p>When the item you want is already taken, you are going to see who got it and for how long.
+                    So you can identify if the person is still using it or if someone just forgot to return it.</p>
+                    <p>You can also ask to be alerted when the item is available. This will notify the active
+                    user that you want to use that item. So the person can return it as soon as they finish using it.</p>',
      
     /**
      * Products
      */
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
+    'products_title' => 'Products',
+    'products_body' => '<p>The <strong>Products</strong> is the screen to include Products and Items.</p>
+                    <p>You can also <strong>add users</strong> to your items from this screen. 
+                    To be able to do that you just need to select the item.</p>
+                    <p class="mb-4">When adding a Product, you can specify a webpage (this is optional).</p>',
      
 ];
