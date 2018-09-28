ce0ddad1e7fb67ceb5a362ff6a5f62cded976fe6
Bruno Fontes <developer@brunofontes.net>
2018-09-28 19:14:29 -0300

    Help Page translated into PTB
    Now the entire website should be translated into PTB.

---
+++ resources/lang/pt-br/help.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Strings from the Help page
+ * They are separeted by help group
+ */
+
+return [
+    'helpTitle' => 'Ajuda...',
+    /**
+     * What is it?
+     */
+    'whatIsIt' => 'O que é?',
+    'siteHelpOthers' => '<p><strong>Share&nbsp;It!</strong> é um site que te ajudar a compartilhar seus items com seus amigos.</p>
+                        <p>Mas antes de qualquer coisa, você precisa entender duas ideias básicas:</p>',
+    'product_item' => '<li><strong>Produto</strong> - Um grupo que contém itens similares</li>
+                        <li><strong>Item</strong> - O item que será compartilhado. Cada item pertence a um <strong>Produto</strong></li>',
+    'examples' => "<strong><em>Exemplos:</em></strong> Você pode criar os seguintes Produtos/Itens:</p>
+                    <p>
+                        <ul>
+                            <li>Livros -> Don Quixote, O guia do mochileiro das galáxias</li>
+                            <li>Filmes Matrix -> Matrix 1, Matrix 2, Matrix 3</li>
+                            <li>Site Y -> <em>SuaConta</em>*</li>
+                        </ul>
+                        <p><em>* Observe que muitos sites não permitem compartilhar sua conta. Leia atentamente o contrato do serviço antes de incluí-lo aqui.</em></p>",
+     
+    /**
+     * Step-by-step
+     */
+    'step_title' => 'Passo a passo',
+    'step_sharing_subtitle' => 'Compartilhando um produto/item',
+    'step_sharing_steps' => '<li>Vá para a página <strong><a href="/product">Produtos</a></strong>;</li>
+                            <li>Inclua o Produto e clique nele;</li>
+                            <li>Inclua um item que pertence ao produto cadastrado e clique nele;</li>
+                            <li>Adicione outras pessoas pelo e-mail que elas se cadastram.</li>',
+
+    'step_sharedItem_subtitle' => 'Usando um item compartilhado',
+    'step_sharedItem_steps' => '<li>Vá até a página <strong><a href="/home">Início</a></strong> (dica: adicione essa página aos seus favoritos);</li>
+                            <li>Clique em <strong>Usar</strong> para usar o item que deseja;</li>
+                            <li>Ao terminar de usar, clique no botão <strong>Devolver</strong>.</li>',
+
+    'step_getAlerted_subtitle' => 'Sendo avisado quando um item está disponível',
+    'step_getAlerted_steps' => '<li>Vá até a página <strong><a href="/home">Início</a></strong> (dica: adicione essa página aos seus favoritos);</li>
+                            <li>Clique no botão <strong>Alertar</strong> próximo ao item que está em uso;</li>
+                            <li>O atual usuário será avisado que você quer usar o item. Quando a pessoa devolver, você será notificado.</li>',
+     
+    /**
+     * Screens
+     */
+    'screens_title' => 'Páginas',
+     
+    /**
+     * Home
+     */
+    'home_title' => 'Início',
+    'home_body' => '<p>A tela <strong>Início</strong> é a sua página principal. Lá você vai <strong>Usar</strong> 
+                    e <strong>Devolver</strong> itens.</p>
+                    <p>Ela exibe todos os itens que estão sendo compartilhados com você ou que você está compartilhando
+                       a menos que você se remova de lá.</p>
+                    <p>Se um item tiver um site cadastrado, ele aparecerá como um link. Então você pode clicar nele para abrir a página.</p>
+                    <p>Quanto o item que você quiser já estiver em uso, você vai ver quem o está usando e por quanto tempo.
+                    Então você pode identificar se a pessoa ainda está usando ou se aparentemente ela esqueceu de retorná-lo.</p>
+                    <p>Você também pode pedir para ser avisado quando o item estiver disponível. Isso vai avisar o usuário ativo
+                    que você está esperando por ele. Então a pessoa pode devolvê-lo assim que ela terminar de usar.</p>',
+     
+    /**
+     * Products
+     */
+    'products_title' => 'Produtos',
+    'products_body' => '<p>A página <strong>Produtos</strong> é a tela usada para incluir Produtos e Itens.</p>
+                    <p>Você também pode <strong>incluir usuários</strong> aos seus items nessa tela. 
+                    Para fazer isso, você só precisa clicar no item que deseja compartilhar e incluir outras pessoas.</p>
+                    <p class="mb-4">Ao adicionar um produto, você pode especificar um site (opcional).</p>',
+     
+];
--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -22,31 +22,24 @@
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><strong>Step-by-step</strong></div>
+                <div class="card-header"><strong>{!! __('help.step_title') !!}</strong></div>
                 <div class="card-body">
                     <div>
-                        <strong>Sharing a product/item</strong>
+                        <strong>{!! __('help.step_sharing_subtitle') !!}</strong>
                         <ol>
-                            <li>Go to the <strong><a href="/product">Products</a></strong> page;</li>
-                            <li>Include the Product and click on it;</li>
-                            <li>Include an Item that belongs to that Product and click on it;</li>
-                            <li>Add other people with their subscribed e-mail address.</li>
+                           {!! __('help.step_sharing_steps') !!} 
                         </ol>
                     </div>
                     <div>
-                        <strong>Using a shared item</strong>
+                        <strong>{!! __('help.step_sharedItem_subtitle') !!}</strong>
                         <ol>
-                            <li>Go to the <strong><a href="/home">Home</a></strong> page (tip: add this page as a bookmark);</li>
-                            <li>Click on <strong>Take</strong> to take the item you want to use;</li>
-                            <li>When you finish using it, click on <strong>Return</strong> button.</li>
+                           {!! __('help.step_sharedItem_steps') !!} 
                         </ol>
                     </div>
                     <div>
-                        <strong>Getting alerted when an item is available</strong>
+                        <strong>{!! __('help.step_getAlerted_subtitle') !!}</strong>
                         <ol>
-                            <li>Go to the <strong><a href="/home">Home</a></strong> page (tip: add this page as a bookmark);</li>
-                            <li>Click on <strong>Alert me</strong> next the taken item;</li>
-                            <li>The active user will be alerted you want to use it later. When the person return it, you will be notified.</li>
+                           {!! __('help.step_getAlerted_steps') !!} 
                         </ol>
                     </div>
                 </div>
@@ -56,23 +49,16 @@
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><strong>Screens</strong></div>
+                <div class="card-header"><strong>{!! __('help.screens_title') !!}</strong></div>
             </div>
         </div>
     </div>
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><strong>Home</strong></div>
+                <div class="card-header"><strong>{!! __('help.home_title') !!}</strong></div>
                 <div class="card-body">
-                    <p>The <strong>Home</strong> is the main screen where you will going to <strong>Take</strong> 
-                    and <strong>Return</strong> items.</p>
-                    <p>It shows all items that were shared with you or that you are sharing, unless you remove yourself from there.</p>
-                    <p>If the item has a website, the items name will become a link, so you can just click on it to open.</p>
-                    <p>When the item you want is already taken, you are going to see who got it and for how long.
-                    So you can identify if the person is still using it or if someone just forgot to return it.</p>
-                    <p>You can also ask to be alerted when the item is available. This will notify the active
-                    user that you want to use that item. So the person can return it as soon as they finish using it.</p>
+                   {!! __('help.home_body') !!} 
                 </div>
             </div>
         </div>
@@ -80,12 +66,9 @@
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><strong>Products</strong></div>
+                <div class="card-header"><strong>{!! __('help.products_title') !!}</strong></div>
                 <div class="card-body">
-                    <p>The <strong>Products</strong> is the screen to include Products and Items.</p>
-                    <p>You can also <strong>add users</strong> to your items from this screen. 
-                    To be able to do that you just need to select the item.</p>
-                    <p class="mb-4">When adding a Product, you can specify a webpage (this is optional).</p>
+                   {!! __('help.products_body') !!} 
                 </div>
             </div>
         </div>
