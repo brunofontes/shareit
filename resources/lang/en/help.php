67d7e34baaef4755354a7991ff4d73b90591a4c0
Bruno Fontes <developer@brunofontes.net>
2018-09-28 02:09:00 -0300

    Adding: help file translation to PTB
    The help file is now translated into PTB

---
+++ resources/lang/en/help.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Strings from the Help page
+ * They are separeted by help group
+ */
+
+return [
+    'helpTitle' => 'Help...',
+    /**
+     * What is it?
+     */
+    'whatIsIt' => 'What is it?',
+    'siteHelpOthers' => '<p><strong>Share&nbsp;It!</strong> is a site dedicated to help you sharing items with others.</p>
+                        <p>But before sharing anything, you just need to understand 2 basic ideas:</p>',
+    'product_item' => '<li><strong>Product</strong> - The category that has some similar items</li>
+                        <li><strong>Item</strong> - The item that will be shared. Every item belongs to a <strong>Product</strong></li>',
+    'examples' => "<strong><em>Examples:</em></strong> You can create the following Categories/Items</p>
+                    <p>
+                        <ul>
+                            <li>Books -> Don Quixote, The Hitchhiker's Guide to the Galaxy</li>
+                            <li>Matrix Movies -> Matrix 1, Matrix 2, Matrix 3</li>
+                            <li>Website Y -> <em>YourAccount</em>*</li>
+                        </ul>
+                        <p><em>* Please note that many websites does not allow sharing your account. Read the site EULA before include it here.</em></p>",
+     
+    /**
+     * Step-by-step
+     */
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+     
+    /**
+     * Screens
+     */
+    '' => '',
+     
+    /**
+     * Home
+     */
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+     
+    /**
+     * Products
+     */
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+     
+];
--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -4,26 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="row"><h1>Help...</h1></div>
+            <div class="row"><h1>{!! __('help.helpTitle') !!}</h1></div>
             <div class="card">
-                <div class="card-header"><strong>What is it?</strong></div>
+                <div class="card-header"><strong>{!! __('help.whatIsIt') !!}</strong></div>
                 <div class="card-body">
-                    <p>
-                        <strong>Share&nbsp;It!</strong> is a site dedicated to help you sharing items with others.
-                    </p>
-                    <p>But before sharing anything, you just need to understand 2 basic ideas:</p>
+                        {!! __('help.siteHelpOthers') !!}
                     <p class="my-4"><ul>
-                        <li><strong>Product</strong> - The category that has some similar items</li>
-                        <li><strong>Item</strong> - The item that will be shared. Every item belongs to a <strong>Product</strong></li>
+                        {!! __('help.product_item') !!}
                     </ul></p>
-                    <p class="my-4"><strong><em>Examples:</em></strong> You can create the following Categories/Items</p>
-                    <p>
-                        <ul>
-                            <li>Books -> Don Quixote, The Hitchhiker's Guide to the Galaxy</li>
-                            <li>Matrix Movies -> Matrix 1, Matrix 2, Matrix 3</li>
-                            <li>Website Y -> <em>YourAccount</em>*</li>
-                        </ul>
-                        <p><em>* Please note that many websites does not allow sharing your account. Read the site EULA before include it here.</em></p>
+                    <p class="my-4">
+                        {!! __('help.examples') !!}
                     </p>
                 </div>
             </div>
