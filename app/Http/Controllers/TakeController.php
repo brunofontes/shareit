1e1c178214cd40ec3746dd82438391a7ed24014a
Bruno Fontes <developer@brunofontes.net>
2018-09-30 21:32:33 -0300

    Refactoring: TakeController returned item Mail moved to a listener
    The TakeController was manually sending the email message
    as well as dealing with item return, so I moved the email message
    to it's own listener, I created an event for it and moved the
    return part to the item model.

--- app/Http/Controllers/TakeController.php
+++ app/Http/Controllers/TakeController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use \App\Item;
 use \App\User;
-use App\Mail\ItemAvailable;
 use Illuminate\Http\Request;
+use App\Events\ReturnItem;
 
 class TakeController extends Controller
 {
@@ -27,19 +27,8 @@ class TakeController extends Controller
     public function delete(Request $request)
     {
         $item = User::loggedIn()->items()->find(request('item'));
-        $waiting_id = $item->waiting_user_id;
-        $item->used_by = null;
-        $item->waiting_user_id = null;
-        $item->save();
-
-        //Send e-mail to waiting user
-        if ($waiting_id) {
-            $user = User::find($waiting_id);
-            \Mail::to($user)->send(
-                new ItemAvailable($user->name, $item)
-            );
-        }
-
+        event(new ReturnItem($item));
+        $item->returnItem();
         return redirect('home');
     }
 }
---
+++ app/Listeners/AlertReturnedItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Listeners;
+
+use Mail;
+use App\User;
+use App\Events\ReturnItem;
+use App\Mail\ItemAvailable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AlertReturnedItem
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Send an email to the user that
+     * is waiting for the item
+     *
+     * @param  ReturnItem  $item
+     * @return void
+     */
+    public function handle(ReturnItem $event)
+    {
+        if ($event->item->waiting_user_id) {
+            $user = User::find($event->item->waiting_user_id);
+            Mail::to($user)->send(
+                new ItemAvailable($user->name, $event->item)
+            );
+        }
+    }
+}
