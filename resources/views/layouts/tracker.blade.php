90bcb44067f544da10d3e8f21be8de9e6d73690f
Bruno Fontes <developer@brunofontes.net>
2021-02-16 20:13:12 -0300

    Fix "@production" message appearing on screen
    It was just for the privacy respect statistics script not appearing on
    test phase.

--- resources/views/layouts/tracker.blade.php
+++ resources/views/layouts/tracker.blade.php
@@ -1,5 +1,3 @@
-@production
 <script type="text/javascript">
 var owa_baseUrl='https://brunofontes.net/owa/';var owa_cmds=owa_cmds||[];owa_cmds.push(['setSiteId','15a38975230dfe7528d647a1419be7f7']);owa_cmds.push(['trackPageView']);owa_cmds.push(['trackClicks']);owa_cmds.push(['trackDomStream']);(function(){var _owa=document.createElement('script');_owa.type='text/javascript';_owa.async=true;owa_baseUrl=('https:'==document.location.protocol?window.owa_baseSecUrl||owa_baseUrl.replace(/http:/,'https:'):owa_baseUrl);_owa.src=owa_baseUrl+'modules/base/js/owa.tracker-combined-min.js';var _owa_s=document.getElementsByTagName('script')[0];_owa_s.parentNode.insertBefore(_owa,_owa_s)}());
     </script>
-@endproduction
