47c68c88601525c18e2fdf78bb3d6b7f54ef03d9
Bruno Fontes <developer@brunofontes.net>
2021-02-17 20:02:27 -0300

    Update Laravel to version 6.*

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -14,8 +14,10 @@
                         </div>
                     @endif
 
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
                     {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('If you did not receive the email') }}, <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                 </div>
             </div>
         </div>
